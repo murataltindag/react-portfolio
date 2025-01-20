3979dfaabe18f070e83d5baf1bacb637bde3cc9a
Murat Altindag <murataltindag3@gmail.com>
2025-01-19 18:03:57 -0600

    Changed Main names and links

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,19 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="https://media.licdn.com/dms/image/v2/D4E03AQGfyXUb5mrWFw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1713260256051?e=1743033600&v=beta&t=rhSmyFZRAsIA0OVIL1dNCLzGjq9z88hV-8FiX1HoYLw" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/murataltindag" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/murat-altindag/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
+          <h1>Murat Altindag</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/murataltindag" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/murat-altindag/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
