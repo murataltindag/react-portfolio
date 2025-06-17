8fa913f55a7cf292642880d27a6d877bae3ea3ff
Murat Altindag <murataltindag3@gmail.com>
2025-06-17 11:33:57 -0500

    Edited fishy

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -19,14 +19,14 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://css-toolbox.lovable.app/" target="_blank" rel="noreferrer"><img src={mock11} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://css-toolbox.lovable.app/" target="_blank" rel="noreferrer"><h2>CSS Toolbox</h2></a>
-                <p>A simple, AI-powered tool to help web designers find their favorite CSS style without coding.</p>
+                <a href="https://github.com/CSC453-Group-Project/LinkedIn" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/CSC453-Group-Project/LinkedIn" target="_blank" rel="noreferrer"><h2>LinkedIn Data Visualization</h2></a>
+                <p>This project aims to transform LinkedIn profile data from survey responses into a and visual format using Pentaho for ETL processes and Power BI for data visualization.</p>
             </div>
             <div className="project">
-                <a href="https://fishy-game.lovable.app/" target="_blank" rel="noreferrer"><img src={mock12} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://fishy-game.lovable.app/" target="_blank" rel="noreferrer"><h2>Fishy Game</h2></a>
-                <p>A simple Fishy Game.</p>
+                <a href="https://docs.google.com/document/d/1hImH6_xDDZlHulTsY1K4KRvIqBw4GmrzDbFgShTiyb8/edit?usp=sharing" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://docs.google.com/document/d/1hImH6_xDDZlHulTsY1K4KRvIqBw4GmrzDbFgShTiyb8/edit?usp=sharing" target="_blank" rel="noreferrer"><h2>Space Invaders</h2></a>
+                <p>A replica of the original arcade Space Invaders, built with C# on Visual Studio. This project was great practice both for Game Development and implementing OOP design patterns.</p>
             </div>
             <div className="project">
                 <a href="https://murataltindag.github.io/debt-snowball-calc/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
@@ -34,22 +34,25 @@ function Project() {
                 <p>A debt snowball calculator built with Flutter. This project helps users to pay off their debts faster by following the debt snowball method.</p>
             </div>
             <div className="project">
-                <a href="https://github.com/CSC453-Group-Project/LinkedIn" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/CSC453-Group-Project/LinkedIn" target="_blank" rel="noreferrer"><h2>LinkedIn Data Visualization</h2></a>
-                <p>This project aims to transform LinkedIn profile data from survey responses into a and visual format using Pentaho for ETL processes and Power BI for data visualization.</p>
+                <a href="https://css-toolbox.lovable.app/" target="_blank" rel="noreferrer"><img src={mock11} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://css-toolbox.lovable.app/" target="_blank" rel="noreferrer"><h2>CSS Toolbox</h2></a>
+                <p>A simple, AI-powered tool to help web designers find their favorite CSS style without coding.</p>
             </div>
-            
             <div className="project">
                 <a href="https://github.com/murataltindag/RoomScape" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://github.com/murataltindag/RoomScape" target="_blank" rel="noreferrer"><h2>RoomScape Project</h2></a>
                 <p>3D local co-op game simulating an escape room with puzzles. Added an inventory system, controller support, and start/end menus.</p>
             </div>
             
+            
             <div className="project">
-                <a><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a><h2>Space Invaders</h2></a>
-                <p>A replica of the original arcade Space Invaders, built with C# on Visual Studio. This project was great practice both for Game Development and implementing OOP design patterns.</p>
+                <a href="https://fishy-game.lovable.app/" target="_blank" rel="noreferrer"><img src={mock12} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://fishy-game.lovable.app/" target="_blank" rel="noreferrer"><h2>Fishy Game</h2></a>
+                <p>A web prototype for a game I am currently developing on Unity.</p>
             </div>
+            
+            
+            
             
         </div>
     </div>
